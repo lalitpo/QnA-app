fabeae457775a29cc199e66ac09d984ddffd3766
lalitpoddar <lalit.dar4@gmail.com>
2023-08-11 17:25:29 +0200

    Play Framework and front end setup

---
+++ app/models/user.scala
@@ -0,0 +1,29 @@
+package models
+
+import scala.collection.mutable.ListBuffer
+import collection.mutable
+
+case class user(username: String, password: String, friends: ListBuffer[String])
+
+object user {
+
+  var users: mutable.Set[user] = mutable.Set(
+    user("lalitpo", "9764", ListBuffer()),
+    user("lalitpod", "6406", ListBuffer()),
+    user("lalitpod09", "6735", ListBuffer()),
+    user("paulina", "9764", ListBuffer()),
+    user("paulinakkf", "6406", ListBuffer()),
+    user("sprintie", "6735", ListBuffer()),
+    user("alex", "6735", ListBuffer())
+  )
+
+  def findallUsers = users.toList
+
+  def createUser(User: user): Unit = {
+    users = users + User
+  }
+
+  def removeUser(User: user): Unit = {
+    users = users - User
+  }
+}
